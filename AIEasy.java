945a6c3d87e23a484d8552c753bc9cda8ba9f7f0
Tyrone Bramwell <tyronebramwell@Tyrones-MacBook-Pro.local>
2014-03-21 11:09:40 +0000

    aieasy

--- AIEasy.java
+++ AIEasy.java
@@ -3,29 +3,28 @@ import java.util.ArrayList;
 
 class AIEasy {
 	
-	public void moves (ArrayList a, Grid b){
+	public ArrayList moves (ArrayList a, Grid b){
 		for (int x=0; x<8;x++){
 			for (int y=0; y<8;y++){
-				a.add(b.getCoordinate(int x, int y));
+				a.add(b.getCoordinate( x, y));
 			}
 		}
+		return a;
 	}
 
+	public void checkMove (){
+		Random rnd = new Random();
+		ArrayList a = new ArrayList();
+		for (int i=0; i<8*8; i++){
+			int x = rnd.nextInt(a.size());
+			System.out.println("num in list "+a.get(x) + " numb gen " + x);
+			a.remove(x);
+		}
+		
+	}
 	
 	public static void main(String[] args) {
-		int x =0;
-		ArrayList m_moves = new ArrayList();
-			
-		Random m_rnd = new Random();
-		Grid b = new Grid();
-		AIEasy ai = new AIEasy();
-		
-		moves(m_moves, b);
-		for (int i=0; i<20; i++){
-			x = m_rnd.nextInt(m_moves.size());
-			System.out.println("num in list "+m_moves.get(x) + " numb gen " + x);
-			m_moves.remove(x);
-		}
+
 	}
 
 }
