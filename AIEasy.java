257380e7768f4966d8218c5ee3878d805f094595
Tyrone Bramwell <tyronebramwell@Tyrones-MacBook-Pro.local>
2014-03-25 13:21:57 +0000

    AIeasy

--- AIEasy.java
+++ AIEasy.java
@@ -1,31 +1,38 @@
 import java.util.Random;
 import java.util.ArrayList;
 
-class AIEasy {
+class AIEasy extends Player {
 	
 	public ArrayList getMoves (ArrayList a){
-		
-		Othello b = new Othello ();
 		
 		for (int x=0; x<8;x++){
 			for (int y=0; y<8;y++){
 				Coordinate  c1 =	new Coordinate (x, y, Game.PlayerTurn.PLAYER1);
-				a.add(c1);
+				
+				
+				if (getGame().isValidMove(c1)==true)
+					a.add(c1);
+				}
 			}
 		}
 		return a;
 	}
 
-	public void checkMove (){
+	public Coordinate makeMove (){
 		Random rnd = new Random();
 		ArrayList a = new ArrayList();
-		for (int i=0; i<8*8; i++){
-			int x = rnd.nextInt(a.size());
-			System.out.println("num in list "+a.get(x) + " numb gen " + x);
-			a.remove(x);
-		}
+		a = getMoves(a);
+		int x = 0;
+		x = rnd.NextInt(a.size());
+		return a.get(x);
 		
 	}
+	
+	public void isYourMove(){}
+
+	public void sendMove(Coordinate move){}
+	
+	public String toString(){}
 	
 	public static void main(String[] args) {
 
