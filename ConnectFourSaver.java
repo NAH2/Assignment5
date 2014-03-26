5a8184686ff61babd9e5ae80497a2fa043253a12
gavin0487 <gavin0487@live.com>
2014-03-26 14:49:40 +0000

    fix

--- ConnectFourSaver.java
+++ ConnectFourSaver.java
@@ -1,10 +1,18 @@
 
 public class ConnectFourSaver extends Saver{
     private final String CONNECTFOUR_PATH = "saveData/connectfour/";
+    private final String CONNECTFOUR_TEST_PATH = "saveDataTest/connectfour/";
+
     
     public ConnectFourSaver(Game g) {
         setGame(g);
         setPath(CONNECTFOUR_PATH);
+        
+        createDirs();
+    }
+    
+    public ConnectFourSaver() {
+        setPath(CONNECTFOUR_TEST_PATH);
         
         createDirs();
     }
--- OthelloSaver.java
+++ OthelloSaver.java
@@ -1,10 +1,17 @@
 
 public class OthelloSaver extends Saver{
     private final String OTHELLO_PATH = "saveData/othello/";
+    private final String OTHELLO_TEST_PATH = "saveDataTest/othello/";
     
     public OthelloSaver(Game g) {
         setGame(g);
         setPath(OTHELLO_PATH);
+        
+        createDirs();
+    }
+    
+    public OthelloSaver() {
+        setPath(OTHELLO_TEST_PATH);
         
         createDirs();
     }
