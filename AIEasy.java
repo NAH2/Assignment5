6ef586195da9e8699ecccecb6566e555867c2430
Tyrone Bramwell <tyronebramwell@Tyrones-MacBook-Pro.local>
2014-03-27 17:48:02 +0000

    AI

--- AIEasy.java
+++ AIEasy.java
@@ -1,30 +1,65 @@
+/**
+ *  \\file -AIEasy.java
+ * 	\author - Tyrone Bramwell
+ *	\date	-20/03/2014
+ *
+ *	\brief This class contains methods to create and run a AI for othello and conect four
+ *		  which randomly picks a a valid move and performs the move.
+ */
 import java.util.*;
 import java.awt.Color;
 
 class AIEasy extends Player {
-	
-	private int m_time = 1500;
-	private boolean m_running;
-	
+	/**
+	* Sets the variable m_running which is used to decide if the AIEasy runs or not
+	*\param run a boolean which selects if the AI should run
+	*/
 	public void SetRun(boolean run){
 		m_running = run;
 	}
-	
+	/**
+	* Sets the the wait time befor which the ai performs a move
+	*\param responseTime a integer for selecting the wait time
+	*/
 	public void SetTime(int responseTime){
 		m_time = responseTime;
 	}
-	
+	/**
+	* Sets the variable m_running which is used to decide if the AIEasy runs or not
+	*/
+	public int getTime(){
+		return m_running;
+	}
+	/**
+	* Sets the the wait time befor which the ai performs a move
+	*\return returns variable m_time which is used for waiting
+	*/
+	public boolean getRun(){
+		return m_time;
+	}
+	/**
+	* Constructor to crate AIEasy with parameters for game type
+	* \param game a type of game for selecting the game
+  	* \param name a string for the player name
+	* \param color a type colour for selecting colour
+	*/
 	public AIEasy(Game game, String name, Color color) {
 		super(game, name, color);
 		m_running = true;
 
 	}
-	
+	/**
+	* Constructor to crate AIEasy with parameters for game type
+	* \param game a type of game for selecting the game
+	*/
 	public AIEasy(Game game) {
 		super(game);
 		m_running = true;
 	}
-	
+	/**
+	* getAvailableMoves creates an array list of valid possiable moves for the game
+	*\return arraylist of all possiable valid moves
+	*/
 	public ArrayList<Coordinate> getAvailableMoves (){
 		ArrayList<Coordinate> a = new ArrayList <Coordinate> ();
 		for (int x=0; x<getGame().getGrid().getGridWidth();x++){
@@ -38,7 +73,10 @@ class AIEasy extends Player {
 		}
 		return a;
 	}
-
+	/**
+	* setAIMove picks a random move from the list and returns a coordinate 
+	*\return coordinate which is then placed into the grid
+	*/
 	public Coordinate setAIMove () throws IndexOutOfBoundsException {
 		Random rnd = new Random();
 	 
@@ -53,7 +91,9 @@ class AIEasy extends Player {
 		return a.get(x);
 		
 	}
-	
+	/**
+	* isYourMove used to run methods which are used to set it as the AI turn
+	*/
 	public void isYourMove() throws InterruptedException {
 		setYourTurn(true);
 		if (!(getGame().getPlayer1() instanceof Human || 
@@ -61,7 +101,10 @@ class AIEasy extends Player {
 		sendMove();
 		}
 	}
-
+	/**
+	* sendMove() a method used to send a valid move to a game of othello or connect
+	 four.
+	*/
 	public void sendMove() throws InterruptedException {
 		new Thread(
 				new Runnable() {
@@ -85,7 +128,11 @@ class AIEasy extends Player {
 				}
 		).start();
 	}
-	
+	/**
+	* sendMove a method used to send a valid move to a game of othello or 
+	connect four.
+	* \param move pass a coordinate object from the grid
+	*/
 	public void sendMove(Coordinate move) throws InterruptedException {
 		class MyThread implements Runnable {
 				Coordinate m_move;
@@ -122,5 +169,13 @@ class AIEasy extends Player {
 		new Thread(r).start();
 
 	}
-
+	public static void main(String[] args) {
+		
+	}
+	/** m_time variable is an integer used to hold the wait time. set to 1500 by 
+	default if not changed */
+	private int m_time = 1500;
+	/** m_running variable is an boolean used to decided if the AIEasy should 
+	run or not*/
+	private boolean m_running;
 }
