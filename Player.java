073dc94bedf42d1931b16bfa8969ceea42ba270a
tomlet <letheby1@live.co.uk>
2014-03-27 14:45:16 +0000

    player conventions

--- Player.java
+++ Player.java
@@ -1,46 +1,74 @@
-/**
- *  @file	-Player.Java
- * 	@author	-C. Hazelton
- * 	@date	-14/02/2014
- * 	
- * 	@brief	Gets move that returns coordinates. Contains name and colour get/set methods.
- */
 import java.awt.Color;
 
+/**
+ *  \file	-Player.Java
+ * 	\author	-C. Hazelton
+ * 	\date	-14/02/2014
+ * 	
+ * 	\brief	Gets move that returns coordinates. Contains name and colour get/set methods.
+ */
 
 public abstract class Player {
-	private String playerName;
-	private Color m_playerColour;
-	private Game m_game;
-	private boolean m_YourTurn = false;
+	
 	/**
 	 * Sets the game that is going to be played
-	 * @param instance of the game class
+	 * \param instance of the game class
 	 */
 	protected void setGame(Game game) {
 		m_game = game;
 	}
 	/**
 	 * Gets the game thats going to be played
-	 * @return game being played
+	 * \return game being played
 	 */
 	protected Game getGame() {
 		return m_game;
 	}
 	/**
 	 * Gets whether the turn is each players'
-	 * @return true or false whether it is the players' turn
+	 * \return true or false whether it is the players' turn
 	 */
 	protected boolean getYourTurn() {
 		return m_YourTurn;
 	}
 	/**
 	 * Sets whether the turn is each players'
-	 * @param set to true if it is players turn, otherwise false
+	 * \param set to true if it is players turn, otherwise false
 	 */
 	protected void setYourTurn(boolean turn) {
 		m_YourTurn = turn;
 	}
+	
+	/**
+	 * Sets the player name
+	 * \param playerName
+	 */
+	public void setPlayerName(String playerName){
+		this.m_playerName=playerName;
+	}
+	/**
+	 * sets the player colour
+	 * \param playerColour
+	 */
+	public void setPlayerColour(Color playerColour){
+		m_playerColour = playerColour;
+	}	
+	/**
+	 * gets the name of the player
+	 * \return player name
+	 */
+	public String getPlayerName(){
+		return m_playerName;
+	}
+	/**
+	 * 	gets the colour of the player
+	 * \return colour
+	 */
+	public Color getPlayerColour(){
+	    //System.out.println(m_playerColour.getRGB());
+		return m_playerColour;
+	}	
+	
 	 /**
 	  * constructor
 	  */
@@ -55,45 +83,17 @@ public abstract class Player {
 		setPlayerName(name);
 		setPlayerColour(color);
 	}
-	/**
-	 * Sets the player name
-	 * @param playerName
-	 */
-	public void setPlayerName(String playerName){
-		this.playerName=playerName;
-	}
-	/**
-	 * sets the player colour
-	 * @param playerColour
-	 */
-	public void setPlayerColour(Color playerColour){
-		m_playerColour = playerColour;
-	}	
-	/**
-	 * gets the name of the player
-	 * @return player name
-	 */
-	public String getPlayerName(){
-		return playerName;
-	}
-	/**
-	 * 	gets the colour of the player
-	 * @return colour
-	 */
-	public Color getPlayerColour(){
-	    //System.out.println(m_playerColour.getRGB());
-		return m_playerColour;
-	}	
+	
 	/**
 	 * abstract method that can be implemented 
 	 * which gets called whenever it is players turn
-	 * @throws InterruptedException 
+	 * \throws InterruptedException 
 	 */
 	public abstract void isYourMove() throws InterruptedException;
 	/**
 	 * abstract method which is called whenever gamewindow receives a move
-	 * @param move that was made
-	 * @throws InterruptedException 
+	 * \param move that was made
+	 * \throws InterruptedException 
 	 */
 	public abstract void sendMove(Coordinate move) throws InterruptedException;
 	
@@ -105,5 +105,10 @@ public abstract class Player {
         System.out.println(playerData);
         return playerData;
     }
+	
+	private String m_playerName;
+	private Color m_playerColour;
+	private Game m_game;
+	private boolean m_YourTurn = false;
 
 }
